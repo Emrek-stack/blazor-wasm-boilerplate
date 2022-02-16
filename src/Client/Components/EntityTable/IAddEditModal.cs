c2c3040e98cab1a1b44dc29c897ecf74bf07b781
Emre Karahan <emre.karahan@gmail.com>
2022-02-16 04:10:45 +0300

    Initial commit

---
+++ src/Client/Components/EntityTable/IAddEditModal.cs
@@ -0,0 +1,9 @@
+﻿namespace FSH.BlazorWebAssembly.Client.Components.EntityTable;
+
+public interface IAddEditModal<TRequest>
+{
+    TRequest RequestModel { get; }
+    bool IsCreate { get; }
+    void ForceRender();
+    bool Validate(object request);
+}
---
+++ src/Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,75 @@
+﻿using FSH.BlazorWebAssembly.Client.Infrastructure.Notifications;
+using FSH.BlazorWebAssembly.Client.Infrastructure.Preferences;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace FSH.BlazorWebAssembly.Client.Shared;
+
+public partial class MainLayout
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+
+    [Parameter]
+    public EventCallback OnDarkModeToggle { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> OnRightToLeftToggle { get; set; }
+
+    [Inject]
+    public NotificationClient Notifications { get; set; } = default!;
+
+    private bool _drawerOpen;
+    private bool _rightToLeft;
+
+    protected override async Task OnInitializedAsync()
+    {
+        // launch the signalR connection in the background.
+        // see https://www.dotnetcurry.com/aspnet-core/realtime-app-using-blazor-webassembly-signalr-csharp9
+#pragma warning disable CS4014
+        Notifications.TryConnectAsync();
+#pragma warning restore CS4014
+
+        if (await ClientPreferences.GetPreference() is ClientPreference preference)
+        {
+            _rightToLeft = preference.IsRTL;
+            _drawerOpen = preference.IsDrawerOpen;
+        }
+    }
+
+    private async Task RightToLeftToggle()
+    {
+        bool isRtl = await ClientPreferences.ToggleLayoutDirectionAsync();
+        _rightToLeft = isRtl;
+
+        await OnRightToLeftToggle.InvokeAsync(isRtl);
+    }
+
+    public async Task ToggleDarkMode()
+    {
+        await OnDarkModeToggle.InvokeAsync();
+    }
+
+    private async Task DrawerToggle()
+    {
+        _drawerOpen = await ClientPreferences.ToggleDrawerAsync();
+    }
+
+    private void Logout()
+    {
+        var parameters = new DialogParameters
+            {
+                { nameof(Dialogs.Logout.ContentText), $"{L["Logout Confirmation"]}"},
+                { nameof(Dialogs.Logout.ButtonText), $"{L["Logout"]}"},
+                { nameof(Dialogs.Logout.Color), Color.Error}
+            };
+
+        var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.Small, FullWidth = true };
+        DialogService.Show<Dialogs.Logout>(L["Logout"], parameters, options);
+    }
+
+    private void Profile()
+    {
+        Navigation.NavigateTo("/account");
+    }
+}
